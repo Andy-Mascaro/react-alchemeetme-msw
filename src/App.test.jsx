75eff5157b002fa625bc1e2d15b1c6ab762689d4
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-04 20:32:07 -0700

    testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,13 +9,11 @@ import App from './App';
 // import rest
 // import setupServer
 
-
-
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: 'Billy',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -29,7 +27,6 @@ const user = {
          
        )
        
-
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
