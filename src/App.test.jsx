f1500c1ed520f365f509b423275014eb89ad27df
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-10 17:46:53 -0700

    moved user to setup test and called it into app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,25 +4,26 @@
 import { screen, render } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { user } from './setupTests'
 import App from './App';
 // 🚨
 // import rest
 // import setupServer
 
-const user = {
-  id: 1,
-  created_at: '2021-12-13T00:17:29+00:00',
-  // 🚨 Add a name here
-  name: 'Vonta',
-  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-  motto: 'Res Non Verba',
-  color: 'crimson',
-};
+// const user = {
+//   id: 1,
+//   created_at: '2021-12-13T00:17:29+00:00',
+//   // 🚨 Add a name here
+//   name: 'Vonta',
+//   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+//   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+//   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+//   motto: 'Res Non Verba',
+//   color: 'crimson',
+// };
 
        const server = setupServer(
-         rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/user`, (req, res, ctx) => res(ctx.json([user]))
+         rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) => res(ctx.json([user]))
        )
          
        )
@@ -41,7 +42,7 @@ test('Should render the header', async () => {
   })
   expect(headerImg).toBeInTheDocument()
   expect(profileName).toBeInTheDocument()
-  screen.debug()
+ 
 })
 
 test('Should render the header with Sasuke 🌬️🔥', async () => {
